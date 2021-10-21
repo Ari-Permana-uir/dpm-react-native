0878191802fa6971e4d4ed1e7c3c6ca317d4a005
Ari-Permana-uir <aripermana150student.uir.ac.id>
2021-10-21 19:51:37 +0700

    merubah informasi app dan json

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image, SafeAreaView } from 'react-native';
+
+
 
 export default function App() {
+ const handlePress = () => console.log("Text pressed");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Hello Mr.Ari, ini aplikasi pertamamu ! perjalanan panjang akan menantimu</Text>
+          
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +18,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignContent: 'center'
+   
   },
 });
